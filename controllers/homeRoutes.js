8ea85d53fcfa17adf7d5c07cb13c78e780debe39
B Laureen <beffylaureen@me.com>
2023-08-22 09:46:16 -0500

    katy found missing /

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -6,21 +6,16 @@ router.get('/', async (req, res) => {
   try {
     // Get all blogs and JOIN with user data
     const blogData = await Blog.findAll({
-      include: [
-        {
-          model: User,
-          attributes: ['name'],
-        },
-      ],
+      include: [User],
     });
 
     // Serialize data so the template can read it
-    const blogss = blogData.map((blog) => blog.get({ plain: true }));
+    const blogs = blogData.map((blog) => blog.get({ plain: true }));
 
     // Pass serialized data and session flag into template
     res.render('homepage', { 
       blogs, 
-      logged_in: req.session.logged_in 
+      // logged_in: req.session.logged_in 
     });
   } catch (err) {
     res.status(500).json(err);
